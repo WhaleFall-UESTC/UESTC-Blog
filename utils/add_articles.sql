a36d75bb2bf03d03be0bb2c6f7410730e4a2b58c
WhaleFall <166845494@qq.com>
2024-05-14 00:41:22 +0800

    Finish /home and /archives

---
+++ utils/add_articles.sql
@@ -0,0 +1,6 @@
+use uestc_blog
+insert into articles (id, title, descr, time) values (1, "Clannad", "Call life as natality, noticing, accompanying, death", "2024-05-08");
+insert into articles (id, title, descr, time) values (2, "Stein;Gate", "宇宙虽有其起源，却没有终结——无限。星球虽也有起源，却因其自身之力走向毁灭——有限。拥有睿智之才最为愚蠢，历史上不胜枚举……", "2024-05-12");
+insert into articles (id, title, descr, time) values (3, "Puella Magi Madoka Magica", "已经没什么好害怕的了，我已经不是孤独一人了", "2024-05-13");
+insert into articles (id, title, descr, time) values (4, "March Comes in Like a Lion", "桐山零，好奇怪的名字呢――不过，跟你还真贴切，因为不就是这样吗？没有家、没有家人、没有去的学校、没有朋友。看吧，容身居所，不管在这世界的任何地方，根本就不存在对吧？", "2024-05-08");
+
