86af4723fb195dd347012ab66be72281e1a03382
WhaleFall <166845494@qq.com>
2024-05-15 00:26:24 +0800

    The demo of Console: 50%

--- utils/init_article.sql
+++ utils/init_article.sql
@@ -5,3 +5,5 @@ CREATE TABLE articles (
                           descr VARCHAR(150) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci,
                           time VARCHAR(25) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci
 );
+
+alter table articles add column content TEXT;
